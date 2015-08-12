7e79c004b14f8357815afd889da12c006071e421
Igor Stepanov <sigorilla@gmail.com>
2015-08-13 00:11:07 +0300

    task-3: done without checking browsers

--- task-3/audio.js
+++ task-3/audio.js
@@ -1,44 +1,101 @@
 var Pleer = (function () {
 
   var context,
+  analyser,
+  bufferLength,
+  dataArray,
+  canvas,
+  gainNode,
+  currSource,
+  playBtn,
+  stopBtn,
+  fileInput,
   dropZone,
-  audioBuffer;
+  audioBuffer,
+  loading,
+  level,
+  loadingFlag;
+
+  var WIDTH = 700,
+      HEIGHT = 200;
 
   var _init = function () {
-    try {
+    // try {
       // Fix up for prefixing
       window.AudioContext = window.AudioContext || window.webkitAudioContext;
       context = new AudioContext();
+      analyser = context.createAnalyser();
+      analyser.fftSize = 256;
+      bufferLength = analyser.frequencyBinCount;
+      dataArray = new Uint8Array(bufferLength);
+
+      canvas = document.getElementById('analyser').getContext('2d');
+      canvas.clearRect(0, 0, WIDTH, HEIGHT);
+
+      gainNode = null;
 
       audioBuffer = null;
+      loadingFlag = true;
+      playBtn = document.getElementById('play');
+      stopBtn = document.getElementById('stop');
+      level = document.getElementById('level');
+      fileInput = document.getElementById('file');
       dropZone = document.getElementById('dragndrop');
+      loading = document.getElementById('loading');
 
       _addListener();
 
-    } catch (e) {
-      alert('Web Audio API is not supported in this browser');
-    }
+    // } catch (e) {
+    //   alert('Web Audio API is not supported in this browser');
+    // }
   };
 
   var _addListener = function () {
-    document.getElementById('play').addEventListener('click', _playAudio);
+    playBtn.addEventListener('click', _playAudio);
+    stopBtn.addEventListener('click', _stopAudio);
+    dropZone.addEventListener('click', _openFileInput);
+    fileInput.addEventListener('change', _handleFileInputSelect, false);
+    level.addEventListener('change', _changeLevel);
     dropZone.addEventListener('dragover', _handleDragOver, false);
     dropZone.addEventListener('drop', _handleFileSelect, false);
+  };
+
+  var _openFileInput = function () {
+    if (loadingFlag) {
+      fileInput.click();
+    }
+  }
+
+  var _handleFileInputSelect = function (event) {
+    if (loadingFlag) {
+      var file = event.target.files[0];
+      _readFile(file);
+    }
+    loadingFlag = false;
   };
 
   var _handleFileSelect = function (event) {
     event.stopPropagation();
     event.preventDefault();
-    var file = event.dataTransfer.files[0];
+    if (loadingFlag) {
+      var file = event.dataTransfer.files[0];
+      _readFile(file);
+    }
+    loadingFlag = false;
+  };
 
+  var _readFile = function (file) {
+    loading.classList.remove('hidden');
     var reader = new FileReader();
 
     reader.onload = function (event) {
       context.decodeAudioData(event.target.result, function (buffer) {
         audioBuffer = buffer;
-        document.getElementById('play').disabled = false;
-        document.getElementById('stop').disabled = false;
+        playBtn.disabled = false;
+        stopBtn.disabled = false;
+        loadingFlag = true;
         document.getElementById('audioTitle').innerHTML = file.name;
+        loading.classList.add('hidden');
       }, function (error) {
         console.log("Error decodeAudioData: ", error);
       });
@@ -54,10 +111,62 @@ var Pleer = (function () {
   };
 
   var _playAudio = function () {
+    if (!context.createGain) {
+      context.createGain = context.createGainNode;
+    }
+    gainNode = context.createGain();
+    gainNode.gain.value = 0.25;
     var source = context.createBufferSource();
     source.buffer = audioBuffer;
-    source.connect(context.destination);
+    source.connect(gainNode);
+    source.connect(analyser);
+    // analyser.connect(distortion);
+    gainNode.connect(context.destination);
+    // source.connect(context.destination);
+    if (!source.start) {
+      source.start = source.noteOn;
+    }
     source.start(0);
+    currSource = source;
+    playBtn.disabled = true;
+    dropZone.classList.add('hidden');
+    _drawBars();
+  };
+
+  var _stopAudio = function () {
+    if (!currSource.stop) {
+      currSource.stop = source.noteOff;
+    }
+    currSource.stop(0);
+    playBtn.disabled = false;
+    dropZone.classList.remove('hidden');
+  };
+
+  var _changeLevel = function (event) {
+    var element = this;
+    var volume = element.value;
+    var fraction = parseInt(element.value) / parseInt(element.max);
+    gainNode.gain.value = fraction * fraction;
+  };
+
+  var _drawBars = function () {
+    drawVisual = requestAnimationFrame(_drawBars);
+
+    analyser.getByteFrequencyData(dataArray);
+
+    canvas.fillStyle = 'rgb(0, 0, 0)';
+    canvas.fillRect(0, 0, WIDTH, HEIGHT);
+    var barWidth = (WIDTH / bufferLength) * 2.5;
+    var barHeight;
+    var x = 0;
+    for (var i = 0; i < bufferLength; i++) {
+      barHeight = dataArray[i]/2;
+
+      canvas.fillStyle = 'rgb(' + (barHeight+100) + ',50,50)';
+      canvas.fillRect(x,HEIGHT-barHeight/2,barWidth,barHeight);
+
+      x += barWidth + 1;
+    }
   };
 
   return {
