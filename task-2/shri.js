47702ae15c3037f97bce73cb65b702b117f7bbe7
Igor Stepanov <sigorilla@gmail.com>
2015-08-13 14:18:26 +0300

    task-2: done

--- task-2/shri.js
+++ task-2/shri.js
@@ -42,46 +42,116 @@ function getData(url, callback) {
 }
 
 /**
- * Ваши изменения ниже
+ * Мои изменения ниже
  */
 var requests = ['/countries', '/cities', '/populations'];
 var responses = {};
 
-for (i = 0; i < 3; i++) {
-    var request = requests[i];
+/**
+ * Получаем население по имени континента, страны или города
+ * @param  {string} name
+ */
+function getPopulationByName(name) {
+    var population = 0;
+    var countries = [];
+    var cities = [];
+
+    countries = getCountriesByContinent(name);
+    if (countries.length === 0) {
+        countries.push(name);
+    }
+
+    cities = getCitiesByCountries(countries);
+    if (cities.length === 0) {
+        cities.push(name);
+    }
+
+    population = getPopulationByCities(cities);
+
+    if (population > 0) {
+        console.log('Population in ' + name + ':', population);
+    } else {
+        console.log('We haven\'t information about ' + name + '.');
+    }
+}
+
+/**
+ * Получаем список стран континента
+ * @param  {string} continent
+ * @return {array}
+ */
+function getCountriesByContinent(continent) {
+    var countries = [];
+    for (var i = 0; i < responses['/countries'].length; i++) {
+        if (responses['/countries'][i].continent === continent) {
+            countries.push(responses['/countries'][i].name);
+        }
+    }
+    return countries;
+}
+
+/**
+ * Получаем список городов страны
+ * @param  {array} countries
+ * @return {array}
+ */
+function getCitiesByCountries(countries) {
+    var cities = [];
+    for (var i = 0; i < responses['/cities'].length; i++) {
+        for (var j = 0; j < countries.length; j++) {
+            if (responses['/cities'][i].country === countries[j]) {
+                cities.push(responses['/cities'][i].name);
+            }
+        }
+    }
+    return cities;
+}
+
+/**
+ * Получаем население городов
+ * @param  {array} cities
+ * @return {integer}
+ */
+function getPopulationByCities(cities) {
+    var population = 0;
+    for (var i = 0; i < responses['/populations'].length; i++) {
+        for (var j = 0; j < cities.length; j++) {
+            if (responses['/populations'][i].name === cities[j]) {
+                population += responses['/populations'][i].count;
+            }
+        }
+    }
+    return population;
+}
+
+/**
+ * Я вынес диалог с пользователем в отдельную функцию, 
+ * чтобы можно было вызывать его не перегружая страницы 
+ * если это будет необходимо
+ */
+function askUser() {
+    var name = window.prompt(
+        'If you want to know population of some area, enter its name.',
+        'Africa'
+    );
+    if (name !== null) {
+        getPopulationByName(name);
+    }
+}
+
+requests.forEach(function (request) {
     var callback = function (error, result) {
+        if (error !== null) {
+            console.log('Error in request:', error);
+            return false;
+        }
+
         responses[request] = result;
-        var l = [];
-        for (K in responses)
-            l.push(K);
 
-        if (l.length == 3) {
-            var c = [], cc = [], p = 0;
-            for (i = 0; i < responses['/countries'].length; i++) {
-                if (responses['/countries'][i].continent === 'Africa') {
-                    c.push(responses['/countries'][i].name);
-                }
-            }
-
-            for (i = 0; i < responses['/cities'].length; i++) {
-                for (j = 0; j < c.length; j++) {
-                    if (responses['/cities'][i].country === c[j]) {
-                        cc.push(responses['/cities'][i].name);
-                    }
-                }
-            }
-
-            for (i = 0; i < responses['/populations'].length; i++) {
-                for (j = 0; j < cc.length; j++) {
-                    if (responses['/populations'][i].name === cc[j]) {
-                        p += responses['/populations'][i].count;
-                    }
-                }
-            }
-
-            console.log('Total population in African cities: ' + p);
+        if (Object.keys(responses).length === 3) {
+            getPopulationByName('Africa');
+            askUser();
         }
     };
-
     getData(request, callback);
-}
+});
